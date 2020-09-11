b3f92d9bbb0d7c016c6368e191b33a6eaee20c21
Oluwatobi Akinrinade <akinrinadeoluwatobiloba@gmail.com>
2020-09-12 00:07:21 +0100

    change

--- src/components/dashboard/homeProfile/homeProfile.js
+++ src/components/dashboard/homeProfile/homeProfile.js
@@ -24,8 +24,8 @@ class HomeProfile extends Component{
                    
                 </Row>   
                 </div>
-                <Row className="main" >
-                    
+                <div className="main">
+                    <div className="first1">
                     <Card border="#E5E5E5" >
                         <Card.Header>Basic Information
                             <span className="edit"><Edit/></span>
@@ -45,7 +45,7 @@ class HomeProfile extends Component{
                         </Card.Text>
                         <Card.Title>Second class (UPPER) </Card.Title>
                         <Card.Text>
-                        Second class (UPPER
+                        Second class (UPPER)
                         </Card.Text>
                         <Card.Title>School</Card.Title>
                         <Card.Text>
@@ -53,9 +53,40 @@ class HomeProfile extends Component{
                         </Card.Text>
                         </Card.Body>
                     </Card>
-                    <Col>
+                    </div>
+                    <div className="second1">
                     <Card className="middle" border="light" >
-                        <Card.Header >Header</Card.Header>
+                        <Card.Header >Summary</Card.Header>
+                        <Card.Body>
+                        <Card.Title> You haven’t added a profile summary yet!</Card.Title>
+                        <Card.Text>
+                            <Add/>
+                            Add Profile Summary
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card className="middle" border="light" >
+                        <Card.Header >Experience</Card.Header>
+                        <Card.Body>
+                        <Card.Title>You haven’t added any job experience</Card.Title>
+                        <Card.Text>
+                            
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card className="middle" border="light" >
+                        <Card.Header >Skills</Card.Header>
+                        <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                            
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card className="middle" border="light" >
+                        <Card.Header >Education 
+                            <p> <Add/>Add more </p>
+                            </Card.Header>
                         <Card.Body>
                         <Card.Title>Light Card Title</Card.Title>
                         <Card.Text>
@@ -64,7 +95,7 @@ class HomeProfile extends Component{
                         </Card.Body>
                     </Card>
                     <Card className="middle" border="light" >
-                        <Card.Header >Header</Card.Header>
+                        <Card.Header >License and Certifications</Card.Header>
                         <Card.Body>
                         <Card.Title>Light Card Title</Card.Title>
                         <Card.Text>
@@ -72,44 +103,14 @@ class HomeProfile extends Component{
                         </Card.Text>
                         </Card.Body>
                     </Card>
-                    <Card className="middle" border="light" >
-                        <Card.Header >Header</Card.Header>
-                        <Card.Body>
-                        <Card.Title>Light Card Title</Card.Title>
-                        <Card.Text>
-                            
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card className="middle" border="light" >
-                        <Card.Header >Header</Card.Header>
-                        <Card.Body>
-                        <Card.Title>Light Card Title</Card.Title>
-                        <Card.Text>
-                            
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card className="middle" border="light" >
-                        <Card.Header >Header</Card.Header>
-                        <Card.Body>
-                        <Card.Title>Light Card Title</Card.Title>
-                        <Card.Text>
-                            
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    </Col>
-                    <Col>
+                    </div>
+                    <div className="third">
                     <Card  className="last" border="#E5E5E5" >
                         <Card.Header>Resume/CV
-                            <span className="edit"><Edit/></span>
                         </Card.Header>
                         <Card.Body>
-                        <Card.Title>Email Address</Card.Title>
-                        <Card.Text>
-                        alandare@gmail.com
-                        </Card.Text>
+                        <Card.Title>Upload a resume</Card.Title>
+                       
                         
                         </Card.Body>
                     </Card>
@@ -118,17 +119,23 @@ class HomeProfile extends Component{
                             <span className="edit"><Edit/></span>
                         </Card.Header>
                         <Card.Body>
-                        <Card.Title>Email Address</Card.Title>
+                        <Card.Title>Years of Experience</Card.Title>
+                        <hr/>
                         <Card.Text>
-                        alandare@gmail.com
+                        0
                         </Card.Text>
-                        
+                        <Card.Title>Availability</Card.Title>
+                        <hr/>
+                        <Card.Text>
+                        Immediate Employment
+                        </Card.Text>
                         </Card.Body>
                     </Card>
-                       
-                    </Col>
-                   
-                </Row>
+
+                    </div>
+
+                </div>
+           
                
             </div>
         )
